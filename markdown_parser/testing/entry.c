29b6d56a06ebada8a70018b6729cb95f435f512e
52617365 <rasengithub@protonmail.com>
2023-01-07 18:37:18 +0200

    all tests added for headings.

--- markdown_parser/testing/entry.c
+++ markdown_parser/testing/entry.c
@@ -1,8 +1,12 @@
 #include "tests.h"
 
 int main(void) {
-    test_next_heading();
-    test_wont_pass();
     // define test function calls here.
+    test_next_h1();
+    test_next_h2();
+    test_next_h3();
+    test_next_h4();
+    test_next_h5();
+    test_next_h6();
 }
 
--- markdown_parser/testing/lexer_test.c
+++ markdown_parser/testing/lexer_test.c
@@ -3,7 +3,38 @@
 #include "../lexer.h"
 #include <stdio.h>
 #include "tests.h"
-void test_next_heading(void) {
+
+void test_next_h1(void) {
+    char* example_h1_heading = "\n# heading";
+    Token token = next(&example_h1_heading);
+
+    if(strcmp(token.lexeme, "heading") == 0) {
+        if(token.type == Heading1) { 
+            LOG_GREEN("PASS");
+        } else {
+            LOG_RED("token.type was not 'Heading1'");
+        }
+    } else {
+        LOG_RED("token.lexeme was not 'heading'");
+    }
+}
+
+void test_next_h2(void) {
+    char* example_h2_heading = "\n## heading";
+    Token token = next(&example_h2_heading);
+
+    if(strcmp(token.lexeme, "heading") == 0) {
+        if(token.type == Heading2) { 
+            LOG_GREEN("PASS");
+        } else {
+            LOG_RED("token.type was not 'Heading2'");
+        }
+    } else {
+        LOG_RED("token.lexeme was not 'heading'");
+    }
+}
+
+void test_next_h3(void) {
     char* example_h3_heading = "\n### heading";
     Token token = next(&example_h3_heading);
 
@@ -17,6 +48,46 @@ void test_next_heading(void) {
         LOG_RED("token.lexeme was not 'heading'");
     }
 }
-void test_wont_pass(void) {
-    LOG_RED("token.lexeme was not 'heading'");
+void test_next_h4(void) {
+    char* example_h4_heading = "\n#### heading";
+    Token token = next(&example_h4_heading);
+
+    if(strcmp(token.lexeme, "heading") == 0) {
+        if(token.type == Heading4) { 
+            LOG_GREEN("PASS");
+        } else {
+            LOG_RED("token.type was not 'Heading4'");
+        }
+    } else {
+        LOG_RED("token.lexeme was not 'heading'");
+    }
+}
+
+void test_next_h5(void) {
+    char* example_h5_heading = "\n##### heading";
+    Token token = next(&example_h5_heading);
+
+    if(strcmp(token.lexeme, "heading") == 0) {
+        if(token.type == Heading5) { 
+            LOG_GREEN("PASS");
+        } else {
+            LOG_RED("token.type was not 'Heading5'");
+        }
+    } else {
+        LOG_RED("token.lexeme was not 'heading'");
+    }
+}
+void test_next_h6(void) {
+    char* example_h6_heading = "\n###### heading";
+    Token token = next(&example_h6_heading);
+
+    if(strcmp(token.lexeme, "heading") == 0) {
+        if(token.type == Heading6) { 
+            LOG_GREEN("PASS");
+        } else {
+            LOG_RED("token.type was not 'Heading6'");
+        }
+    } else {
+        LOG_RED("token.lexeme was not 'heading'");
+    }
 }
