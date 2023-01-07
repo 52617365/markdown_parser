3c15469f4b9143ba452c299a042f3a4b2428786a
52617365 <rasengithub@protonmail.com>
2023-01-07 18:40:41 +0200

    updated the build scripts to run after.

--- test.bat
+++ test.bat
@@ -3,4 +3,5 @@
 mkdir ..\build
 pushd ..\build
 cl -Zi /Wall ..\markdown_parser\testing\entry.c ..\markdown_parser\testing\lexer_test.c ..\markdown_parser\lexer.c ..\markdown_parser\copy.c ..\markdown_parser\token.c
+entry.exe
 popd
